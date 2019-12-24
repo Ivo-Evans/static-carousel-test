991bd3f15b54aca01ce857cff7e3d85289cabe98
Ivo Evans Storrie <ievans.storrie@gmail.com>
2019-12-24 13:32:57 +0000

    link js file. changeImage removes every visible_image class from images

--- carouselScript.js
+++ carouselScript.js
@@ -2,5 +2,5 @@ const images = document.querySelectorAll(".image");
 const currentImage = 0;
 
 function changeImage(to) {
-
+  Array.from(images).forEach(im => im.classList.remove("visible_image"))
 }
