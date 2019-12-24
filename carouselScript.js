a839786f8a63c45d84790209ec25861df4be84b9
Ivo Evans Storrie <ievans.storrie@gmail.com>
2019-12-24 14:19:50 +0000

    create constants for right button and left button

--- carouselScript.js
+++ carouselScript.js
@@ -2,5 +2,11 @@ const images = document.querySelectorAll(".image");
 const currentImage = 0;
 
 function changeImage(to) {
-  Array.from(images).forEach(im => im.classList.remove("visible_image"))
+  Array.from(images).forEach(im => im.classList.remove("visible_image"));
+  Array.from(images).forEach(im => im.classList.add("hidden_image"));
+  images[to].classList.remove("hidden_image");
+  images[to].classList.add("visible_image");
 }
+
+const rightButton = document.getElementById("rightButton");
+const leftButton = document.getElementById("leftButton");
