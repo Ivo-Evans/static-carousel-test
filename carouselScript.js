bc4aa7b2ac0a4ebffd9e48299e05939c5292db0a
Ivo Evans Storrie <ievans.storrie@gmail.com>
2019-12-24 15:30:04 +0000

    reorganise code

--- carouselScript.js
+++ carouselScript.js
@@ -1,14 +1,6 @@
 const images = document.querySelectorAll(".image");
 let currentImage = 0; // should start at 0;
 
-function changeImage(to) {
-  Array.from(images).forEach(im => im.classList.remove("visible_image"));
-  Array.from(images).forEach(im => im.classList.add("hidden_image"));
-  images[to].classList.remove("hidden_image");
-  images[to].classList.add("visible_image"); // perhaps this process could be optimised, bearing in mind CSS class precedence (a given thing can have two incompatible classes and the later declaration takes precedence)
-  currentImage = to;
-}
-
 const rightButton = document.getElementById("rightButton");
 const leftButton = document.getElementById("leftButton");
 
@@ -24,10 +16,19 @@ function moveLeft() {
   currentImage <= 0 ? changeImage(images.length - 1) : changeImage(currentImage - 1);
 }
 
-const playPauseButton = document.getElementById("playPause");
-playPauseButton.addEventListener('click', playPause);
+function changeImage(to) {
+  Array.from(images).forEach(im => im.classList.remove("visible_image"));
+  Array.from(images).forEach(im => im.classList.add("hidden_image"));
+  images[to].classList.remove("hidden_image");
+  images[to].classList.add("visible_image"); // perhaps this process could be optimised, bearing in mind CSS class precedence (a given thing can have two incompatible classes and the later declaration takes precedence)
+  currentImage = to;
+}
+
 let play = setInterval(() => moveRight(), 5000);
 let playing = true
+
+const playPauseButton = document.getElementById("playPause");
+playPauseButton.addEventListener('click', playPause);
 
 function playPause() {
   playing ? clearInterval(play) : play = setInterval(() => moveRight(), 5000) ;
